8cb4818c19ef777ee4fa575171abb065c48d37d8
xiannac <xiannacolon7@gmail.com>
2024-04-12 19:54:37 -0400

    added initial javascript

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+function generateQuote(event) {
+  event.preventDefault();
 
+  new Typewriter("#quote", {
+    strings: "Today will be a productive day :]",
+    autoStart: true,
+    cursor: "",
+    delay: 18,
+  });
+}
+
+let quoteFormElement = document.querySelector("#quote-form");
+quoteFormElement.addEventListener("submit", generateQuote);
